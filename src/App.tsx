9aa301decb32deb1c1e86466dabae381a298f42d
YegorOsipov <gogsyan11@gmail.com>
2022-03-23 00:33:53 +0300

    added yo

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Yo <code>src/App.tsx</code> Hello world!!!.
         </p>
         <a
           className="App-link"
